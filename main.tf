0907f28b1f6545fed5d6c35a6bc32374181d3e97
Parvez Rafi <rafiparvez@gmail.com>
2020-03-05 16:04:35 -0500

    added S3 buckets for data lake

--- main.tf
+++ main.tf
@@ -6,3 +6,26 @@ provider "aws" {
   version = "~> 2.6"
 
 }
+# restrict public access to buckets and objects
+resource "aws_s3_account_public_access_block" "block_s3_bucket_public_access" {
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+  block_public_acls       = true
+  block_public_policy     = true
+}
+
+# Create buckets for data lake
+resource "aws_s3_bucket" "landing_zone_bucket" {
+  bucket = "${var.dl_bucket_prefix}.landing"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "refined_zone_bucket" {
+  bucket = "${var.dl_bucket_prefix}.refined"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "project_zone_bucket" {
+  bucket = "${var.dl_bucket_prefix}.project"
+  acl    = "private"
+}
--- variables.tf
+++ variables.tf
@@ -1,4 +1,14 @@
 variable "aws_region" {
   description = "The AWS region in which all resources will be created"
   type        = string
+  default     = "us-east-1"
 }
+
+variable "dl_bucket_prefix" {
+  description = "The prefix for s3 buckets within data lake"
+  type        = string
+  default     = "com.bb.data"
+}
+
+
+
